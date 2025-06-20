68f38646035e76b4446055234359f37ddbe958fc
1kotik <lyoshakotik@gmail.com>
2025-06-20 18:25:19 +0300

    initial commit

---
+++ user-service/src/test/java/by/kotik/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.kotik.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
